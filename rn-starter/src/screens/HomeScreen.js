040883bcd95b3b9b71eef04873b00c170978cc64
Stefan <stefanmitov8@gmail.com>
2021-10-20 00:36:52 +0300

    Complete Section 5

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 const HomeScreen = ({ navigation}) => {
   return (
     <View>
-      <Text style={styles.text}>HomeScreen</Text>
+      <Text style={styles.text}>Home Screen</Text>
       <Button 
         title="Go to Components Demo" 
         onPress={() => navigation.navigate('Components')}
@@ -12,6 +12,10 @@ const HomeScreen = ({ navigation}) => {
       <Button 
         title="Go to List Demo"
         onPress={() => navigation.navigate('List')}
+      />
+      <Button 
+        title="Go to Image Demo"
+        onPress={() => navigation.navigate('Image')}
       />
     </View>
   );
